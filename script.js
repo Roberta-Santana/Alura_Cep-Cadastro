c835c84960d4265b813358beac10fa2fc0bf54b8
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-12 20:15:41 -0300

    usando catch

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(convesao=> convesao.json())
+.then(r => console.log(r))
+.catch(erro => console.log(erro));
+
+console.log(consultaCEP);
