c9d9f53ee3cdd98e4de0f43f2304931f3f790b3b
Roberta Santana <g.roberta.s@hotmail.com>
2024-06-13 09:51:56 -0300

    finally

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-/* const minhaPromise = new Promise((resolve, reject) => { // Simulando uma operação assíncrona que falha
-    const sucesso = false;
-    if (sucesso) {resolve('Operação bem-sucedida!');
-    } else {reject('Operação falhou!');}});
-  
-  minhaPromise
-    .then(result => {console.log('Sucesso:', result);})
-    .catch(error => { console.error('Erro:', error);}); */
-
-var consultaCEP = new Promise((resolve, reject) => {// Simulando uma operação de busca de CEP
+/* var consultaCEP = new Promise((resolve, reject) => {// Simulando uma operação de busca de CEP
   let sucesso = true; // Simulando sucesso ou falha
   if (sucesso) {
     resolve(fetch('https://viacep.com.br/ws/01001250/json/'));
   } else {
     reject('operação falhou!');
   }
-});
+}); */
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/');
+
 consultaCEP.then(convesao=> convesao.json())
 .then(r => {
     if(r.erro){ //casos com formato de CEP válido, mas valor inexistente
         throw Error('Esse CEP não existe!')
-    } else
-    console.log(r)})
-.catch(erro => console.log(erro));
+    } else {console.log(r)}
+})
+.catch(erro => console.log(erro))
+.finally(msg => console.log("Procesamento concluído"));
 
 console.log(consultaCEP);
